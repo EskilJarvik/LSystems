2b493277f75a699ce945549a93e34a4736d7b82b
ttepmreh <hermpett@elev.rogfk.no>
2023-04-21 11:10:49 +0200

    a

--- draw.py
+++ draw.py
@@ -7,24 +7,46 @@ def draw_plant(plante):
     displayHeight = 300
 
     canvas = Canvas(width = displayWidth, height = displayHeight)
+    canvas.stroke_style = "black"
 
     string = plante.string 
     x = 0
     y = 0
 
+    stack = []
+
     movment = {
         "F": [0,5],
-        "+": [4,1],
-        "-": [-4,1]
+        "X": [0,5],
+    }
+
+    angle = {
+        "+": 20,
+        "-": -20
     }
 
     for c in string:
-        if c in movment:
+        if c in angle:
+            angle += int(angle[c])
+
+        elif c in movment:
             move = movment[c]
+
+            # calculate angle
+
+            canvas.stroke_line(x, y, x+move[0], y+move[1])
             x = move[0]
             y = move[1]
 
-            
+        elif c == "[":
+            goTo = stack(-1)
+            x = goTo[0]
+            y = goTo[1]
+            angle = goTo[2]
+            stack.pop()
+
+        elif c == "[":
+            stack.append([x,y, angle])
 
     if error:
         return False
