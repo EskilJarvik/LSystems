b7d6bf177e2ca5288470976275a478db934649ab
EskilJarvik <eskil.jrvik@gmail.com>
2023-04-24 12:58:32 +0200

    changes

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
 from draw import *
 from plant_class import *
 
-plante = plant("plant 1", 4, "F+F--+F")
-draw_plant(plante)
+plant_names = [
+    "wonk",
+    "y"
+]
+
+plant_axioms = [
+    "a",
+    "y"
+]
+
+plant_rulesets = [
+    {
+        'F': ">F<",
+        'a': "F[+x]Fb",
+        'b': "F[-y]Fa",
+        'x': "a",
+        'y': "b"
+    },
+    {
+        'X': "X[-FFF][+FFF]FX",
+        'Y': "YFX[+Y][-Y]"
+    }
+]
+
+plant_type = int(input("What plant"))
+plant = plants(plant_names[plant_type], 0, plant_axioms[plant_type]) 
+
+time = 0
+end_time = 3
+
+while time < end_time:
+    plant.grow(plant_rulesets[plant_type])
+    time = round(time + 0.1)
+
+plant.draw()
