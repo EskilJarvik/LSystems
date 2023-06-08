718ec792728727e3528d9e9f25519fb2b9125c24
ttepmreh <126167875+ttepmreh@user.noreply.github.com>
2023-06-08 14:47:03 +0200

    indeed

--- test.py
+++ test.py
@@ -12,13 +12,9 @@ def copy_plant(plant_to_copy):
 
 plant_types = [
     #     name,     age,   string,    angle,  x, y, z,  water
-    plant("test",    0,   "(10,0)F",  22.5,   0, 0, 0,  100), #0
-    plant("wonk",    0,   "a",        90,     0, 0, 0,  100), #1
-    plant("fractal", 0,   "0",        90,     0, 0, 0,  100), #2
-    plant("plant",   0 ,  "X",        25,     0, 0, 0,  100), #3
-    plant("amogus",  0,   "X",        180,    0, 0, 0,  100), #4
-    plant("love",    0,   "F",        90,     0, 0, 0,  100), #5
-    plant("Trekant", 0,   "A",        60,     0, 0, 0,  100)  #6
+    plant("uno",     0,   "(10,0)F",  22.5,   0, 0, 0,  100), #0
+    plant("dos",     0,   "(10,0)F",  25,     0, 0, 0,  100), #1
+    plant("tres",    0,   "(7,0)F",   20,     0, 0, 0,  100), #2
 ]
 
 rulesets = [
@@ -26,43 +22,22 @@ rulesets = [
         'F': "X[(10,0,&+)F][(10,0,^+)F][(10,0,-^)F][(10,0,&)F]"
     },
     {
-        'F': ">F<",
-        'a': "F[+x]Fb",
-        'b': "F[-y]Fa",
-        'x': "a",
-        'y': "b"
+        'F': "X[(8,0,&-)F][(9,0,++)F][(10,0,+&)F][(8,0,^-)F]"
     },
     {
-        'F': "FF",
-        'X': "F[+X]-X"
-    },
-    {
-        'A': "FXF",
-        'B': "XXX"
-    },
-    {
-        'F': "F+F-F-F+F",
-    },
-    {
-        'F': "F-X+F+X-F",
-        'X': "XX"
-    },
-    {
-        'F': "F-X+F+X-F",
-        'X': "XX"
+        'F': "X[(9,0,^+)F][(10,0,^-)F][(9,0,--)F][(6,0,^)F]"
     }
 ]
 
 number_of_plants = int(input("Number of plants: "))
-plant_index = int(input("Plant index: "))
 end_gen = int(input("Draw gen: "))
 generations = end_gen*12
 
 offset = 50
+plant_index = 0
 
 plant_array_side_length = math.ceil(number_of_plants**(1/2))
 plants = []
-t = 0
 for i in range(plant_array_side_length):
     plant_row = []
     for j in range(plant_array_side_length):
@@ -70,15 +45,12 @@ for i in range(plant_array_side_length):
             plant_row.append( copy_plant(plant_types[plant_index]) )
             plant_row[j].xPos = (i - (plant_array_side_length / 2)) * ( offset + random.randint(offset / -10, offset / 10) )
             plant_row[j].zPos = (j - (plant_array_side_length / 2)) * ( offset + random.randint(offset / -10, offset / 10) )
-            print(t)
-            print(plant_row[j].xPos)
-            print(plant_row[j].zPos)
-            t += 1
         else:
             break
     plants.append(plant_row)
 
-waterIntake = [150,150,600,600,600,1150,1150,1150,250,250,250,150]
+waterIntake = [150,150,500,500,500,1000,1000,1000,250,250,250,150]
+monthName = ["Januar", "Februar", "Mars", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Desember"]
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
@@ -89,13 +61,12 @@ def update(frame):
     i = frame
     ax.cla()
     ax.set_box_aspect(aspect=(1, 1, 1))
-    ax.set_xlim(-axis_length, axis_length)
+    ax.set_xlim(-axis_length/2, axis_length/2)
     ax.set_ylim(-axis_length, axis_length)
-    ax.set_zlim(0, 2 * axis_length)
-    ax.set_xlabel('X')
-    ax.set_ylabel('Z')
-    ax.set_zlabel('Y')
+    ax.set_zlim(0, axis_length)
+    ax.axis('off')
     month = i-12*((i-1)//12)
+    ax.text2D(0.4, 0.8, monthName[month-1], transform=ax.transAxes)
 
     for j in range(plant_array_side_length):
         for k in range(plant_array_side_length):
@@ -103,7 +74,7 @@ def update(frame):
                 plants[j][k].water = waterIntake[month-1]
                 if month not in [12,1,2]:
                     plants[j][k].grow(rulesets[plant_index], month)
-                    plants[j][k].draw(ax, month)
+                plants[j][k].draw(ax, month)
     
 plantAnimation = animation.FuncAnimation(fig, update, frames=range(1, generations + 1), interval=1000, repeat=False)
 plt.show()
